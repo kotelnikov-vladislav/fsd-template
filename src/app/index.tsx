35072b377cf20fff09ecd91866ce15a1504785fb
kotelnikov-vladislav <vladislav.kotelnikov@internet.ru>
2024-01-06 16:25:20 +0300

    add prettier

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,5 @@
 const App = () => {
-    return (
-        <main>
-            Hello FSD!
-        </main>
-    )
-}
+    return <main>Hello FSD!</main>;
+};
 
 export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: '/src/main.tsx'
-    }
-  },
-  resolve: {
-  },
-  plugins: [react()],
-})
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@': '/src/',
+        },
+    },
+});
